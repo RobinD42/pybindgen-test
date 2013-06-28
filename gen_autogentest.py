ce6120d3485ab7210f6329a75f6a4ea3d75745e2
Robin Dunn <robin@alldunn.com>
2013-06-28 15:58:18 -0500

    change file names, add wrapper code

--- gen_autogentest.py
+++ gen_autogentest.py
@@ -3,19 +3,24 @@ Test script for generating a wrapper for the code in testclasses.h using gccxml.
 """
 
 import sys
+import os
 
 import pybindgen
 from pybindgen import FileCodeSink
 from pybindgen.gccxmlparser import ModuleParser
 
-NAME = 'testclasses'
-SRC = 'testclasses.h'
-DEST = 'testclasses_wrap.cpp'
+NAME = 'autogentest'
+SRC = 'autogentest.h'
+DEST = 'autogentest_wrap.cpp'
+
+# TODO: look for this on the PATH, for now I've just installed it in my VirtualEnv
+GCCXML = os.path.join(sys.prefix, 'bin', 'gccxml')  
+
 
 def my_module_gen():
     module_parser = ModuleParser(NAME)
-    module = module_parser.parse([SRC], 
-                                 gccxml_options=dict(gccxml_path='./PyVE/bin/gccxml'))
+    module = module_parser.parse(
+        [SRC], gccxml_options=dict(gccxml_path=GCCXML))
     module.add_include('"%s"' % SRC)
 
     output = open(DEST, 'w')
