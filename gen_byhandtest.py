5647a1b3725ed58895e882d013e33f53430c2ed7
Robin Dunn <robin@alldunn.com>
2013-06-29 16:50:51 -0500

    More by-hand work

--- gen_byhandtest.py
+++ gen_byhandtest.py
@@ -8,8 +8,11 @@ import os
 from pybindgen import ReturnValue, Parameter, Module, Function, FileCodeSink
 from pybindgen import CppMethod, CppConstructor, CppClass, Enum
 
+import pybindgen.settings
+pybindgen.settings.automatic_type_narrowing = True
+
 NAME = 'byhandtest'
-SRC = 'autogentest.h'
+SRC  = 'autogentest.h'
 DEST = 'byhandtest_wrap.cpp'
 
 
@@ -22,7 +25,49 @@ def my_module_gen():
     A.add_method('virtualMethod1', None, [], is_virtual=True, is_pure_virtual=True)
     A.add_method('virtualMethod2', None, [], is_virtual=True)
 
+
+    
+    B = module.add_class('B', parent=A)
+    B.add_constructor([])
+    B.add_constructor([Parameter.new('int', 'a'), Parameter.new('int', 'b')])
+    B.add_method('virtualMethod1', None, [], is_virtual=True)
         
+    B.add_method('overloadedMethod', None, [])
+    B.add_method('overloadedMethod', None, [Parameter.new('int', 'a'), Parameter.new('int', 'b')])
+    B.add_method('overloadedMethod', None, [Parameter.new('double', 'd')])
+           
+    B.add_method('defaultArgs', None, [Parameter.new('int', 'a', default_value='0'), 
+                                       Parameter.new('int', 'b', default_value='123')])
+
+    #bool operator==(const B& other) { return false; }
+    #bool operator!=(const B& other) { return true; }
+
+    #B& operator+(const B& other) { return *this; }
+
+    #// anything special done for these?
+    #int __len__() { return 0; }  // yes, added to sequence methods structure
+    #int __int__() { return 0; }  // no but the method is still there
+    
+    #operator bool() { return true; } // no
+    
+    #static void staticMethod() {}
+    
+    #int m_publicAttribute;
+                                
+
+
+    C = module.add_class('C', parent=B)
+    B.add_constructor([])
+    B.add_constructor([Parameter.new('int', 'a'), Parameter.new('int', 'b')])
+    B.add_method('returnBaseClassPtr', ReturnValue.new('A*',
+                                                       #caller_owns_return=True
+                                                       reference_existing_object=True
+                                                       ), [])
+        
+    #A* returnBaseClassPtr() { return this; }    
+    #void baseClassParameterPtr(const A* ptr) {}
+    #void baseClassParameterRef(const A& ptr) {}
+
     
     output = open(DEST, 'w')
     module.generate(FileCodeSink(output))
